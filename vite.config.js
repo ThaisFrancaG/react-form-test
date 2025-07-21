040ecae1b9359083066d6eb244b25ecdb9c0133c
Thais Franca <tfrancag@gmail.com>
2025-07-21 20:57:32 -0300

    chore: Update to allow publish

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'tsx',
-  },
+  base: '/react-form-tes/', // <-- nome EXATO do seu repositório
 });
